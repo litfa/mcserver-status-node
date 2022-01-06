18712447ffafa1b349bfff5e04aa475bdef2151a
xingHome <xingqq2233@qq.com>
2022-01-06 23:40:45 +0800

    log4js

--- app.js
+++ app.js
@@ -2,19 +2,29 @@
  * @Author: litfa 
  * @Date: 2022-01-05 17:01:56 
  * @Last Modified by: litfa
- * @Last Modified time: 2022-01-05 20:12:39
+ * @Last Modified time: 2022-01-06 23:25:27
  */
+// log
+global.logger = {}
+require('./module/log')
+
 const express = require('express')
 
+logger.info('初始化配置文件')
 global.CONFIG = require('./config.js')()
 
-const app = express()
-
+// 开启定时任务
+logger.info('开启定时任务')
 require('./module/schedule')
+
+logger.info('初始化 express 模块')
+const app = express()
 
 app.use('/api/getStatus', require('./router/getStatus.js'))
 app.use('/api/getStatusLog', require('./router/getStatusLog'))
+app.use('/', express.static('./dist/'))
 
 app.listen(CONFIG.port, () => {
-  // logger.info()
+  logger.info(`监听端口 ${CONFIG.port}`)
+  logger.info(`http://127.0.0.1:${CONFIG.port}`)
 })
--- module/schedule.js
+++ module/schedule.js
@@ -2,7 +2,7 @@
  * @Author: litfa 
  * @Date: 2022-01-05 17:01:51 
  * @Last Modified by: litfa
- * @Last Modified time: 2022-01-06 23:15:15
+ * @Last Modified time: 2022-01-06 23:40:29
  */
 const schedule = require('node-schedule')
 const getAllServersStatus = require('./getAllServersStatus')
@@ -11,6 +11,7 @@ const path = require('path')
 
 let dataLogFilePath = path.join(__dirname, '../serverLog/log.json')
 schedule.scheduleJob(CONFIG.schedule, async () => {
+  logger.info('开始获取数据')
   let data = await getAllServersStatus({ favicon: false })
 
   let dataLog = fs.readFileSync(dataLogFilePath)
@@ -18,12 +19,9 @@ schedule.scheduleJob(CONFIG.schedule, async () => {
 
   dataLog.push({ date: Date.now(), data })
   // 移除多余数据
-  console.log(dataLog)
   if (dataLog.length > CONFIG.storeDataNum && CONFIG.storeDataNum != -1) {
     dataLog = dataLog.slice(dataLog.length - CONFIG.storeDataNum)
   }
-  console.log(dataLog.length > CONFIG.storeDataNum)
-  console.log(CONFIG.storeDataNum)
-  console.log(dataLog.length)
   fs.writeFileSync(dataLogFilePath, JSON.stringify(dataLog))
+  logger.info('获取完成')
 })
