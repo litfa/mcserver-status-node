989370a74bdc11fda9c9dd316c6a37080fe6fb8a
xingHome <xingqq2233@qq.com>
2022-01-06 23:18:22 +0800

    保留的数据数量

--- module/schedule.js
+++ module/schedule.js
@@ -2,7 +2,7 @@
  * @Author: litfa 
  * @Date: 2022-01-05 17:01:51 
  * @Last Modified by: litfa
- * @Last Modified time: 2022-01-05 22:38:50
+ * @Last Modified time: 2022-01-06 23:15:15
  */
 const schedule = require('node-schedule')
 const getAllServersStatus = require('./getAllServersStatus')
@@ -17,5 +17,13 @@ schedule.scheduleJob(CONFIG.schedule, async () => {
   dataLog = JSON.parse(dataLog)
 
   dataLog.push({ date: Date.now(), data })
+  // 移除多余数据
+  console.log(dataLog)
+  if (dataLog.length > CONFIG.storeDataNum && CONFIG.storeDataNum != -1) {
+    dataLog = dataLog.slice(dataLog.length - CONFIG.storeDataNum)
+  }
+  console.log(dataLog.length > CONFIG.storeDataNum)
+  console.log(CONFIG.storeDataNum)
+  console.log(dataLog.length)
   fs.writeFileSync(dataLogFilePath, JSON.stringify(dataLog))
 })
