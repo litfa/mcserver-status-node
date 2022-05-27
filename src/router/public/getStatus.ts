56890752cde939221278dcb60afacc1b6b0f825d
Litfa <admin@litf.com.cn>
2022-05-27 22:09:34 +0800

    feat: id 查询

--- src/router/public/getStatus.ts
+++ src/router/public/getStatus.ts
@@ -5,31 +5,48 @@ const router = Router()
 
 const sql = `
 select
+    id,
     date,
     status,
+    motd,
     max,
     online
 from status
-where date > date_sub(now(), interval ? hour)
-order by date desc
-limit 36
+where
+    date > date_sub(now(), interval ? hour)
+    and
+    id=?
+order by date
+limit 144
 `
+
+// 验证id参数
+router.post('/*', (req, res, next) => {
+  const id = req.body.id
+  if (id === undefined || typeof id != 'number') {
+    return res.send({ code: 403, msg: '参数有误' })
+  }
+  next()
+})
 
 // 6h 24h 7d 30d
 router.post('/6h', async (req, res) => {
-  const [err, request] = await query(sql, 6)
+  const id = req.body.id
+  const [err, request] = await query(sql, [6, id])
   if (err) return res.send({ code: 500 })
   res.send({ code: 200, data: request })
 })
 
 router.post('/24h', async (req, res) => {
-  const [err, request] = await query(sql, 24)
+  const id = req.body.id
+  const [err, request] = await query(sql, [24, id])
   if (err) return res.send({ code: 500 })
   res.send({ code: 200, data: request })
 })
 
 // 展示近七天 最高人数 平均人数 可用率
 router.post('/7d', async (req, res) => {
+  const id = req.body.id
   const sql = `
   select
   #      按天查询
@@ -41,11 +58,14 @@ router.post('/7d', async (req, res) => {
   #      平均
          avg(online) as online
   from status
-  where date > date_sub(now(), interval 7 day)
+  where
+      date > date_sub(now(), interval 7 day)
+      and
+      id=?
   group by date_format(date, '%Y-%m-%d')
   order by date desc
   `
-  const [err, request] = await query(sql)
+  const [err, request] = await query(sql, id)
   if (err) return res.send({ code: 500 })
   res.send({ code: 200, data: request })
 })
